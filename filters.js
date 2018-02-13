9499ba058d9106a18c49becd5745ec5965721685
p <p>
2018-02-12 22:56:37 -0800

    Updated for 3.0
    Removed unnecessary files

--- filters.js
+++ filters.js
@@ -1,7 +1,7 @@
 
 var filters = {
-        gameMode: "ranked",
+        gameMode: "5v5_pvp_casual",
         createdStart: new Date(new Date().setDate(new Date().getDate() - 27)).toISOString(), //This returns the ISO string date from 27 days ago. The max search date range is 28 days.
-        patch: "2.12",
+        patch: "3.0",
         apiKey: "Bearer aaa.bbb.ccc"
 }
